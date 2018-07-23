f1bc6e45dd3d687f1f0e9852d242623f06469b62
Ariel Vernaza <ariel.darkserver@gmail.com>
2018-07-23 01:44:14 -0500

    ros basic topics structure

--- retro_gym_server.py
+++ retro_gym_server.py
@@ -7,34 +7,51 @@ import numpy as np
 import imutils
 from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage
+from geometry_msgs.msg import Twist
 #check world debug
 
 
 env = retro.make(game='SonicTheHedgehog-Genesis', state='GreenHillZone.Act1')
+image_pub = rospy.Publisher('world_observation/image_raw', CompressedImage, queue_size=10)
 
-def open_world():
-    rospy.init_node('domain_server', anonymous=True)
-    image_pub = rospy.Publisher('world_observation/image_raw', CompressedImage, queue_size=10)
-    env.reset()
-    action = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        #GYM RENDER AS IMAGE
-        img = env.render(mode='rgb_array')
-        # ROTATE THE IMAGE THE MATRIX IS 90 grates and mirror
-        img = np.flipud(np.rot90(img))
-        image_np = imutils.resize(img, width=500)
-        # Publish new image
-        msg = CompressedImage()
-        msg.header.stamp = rospy.Time.now()
-        msg.format = "jpeg"
-        compressed_images = cv2.imencode('.jpg', image_np)
-        msg.data = np.array(compressed_images[1]).tostring()
-        image_pub.publish(msg)
-        obs, rew, done, info = env.step(action)
+def key_action(vel_msg):
+    #["B", "A", "MODE", "START", "UP", "DOWN", "LEFT", "RIGHT", "C", "Y", "X", "Z"]
+    keys = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    if vel_msg.linear.x == -1:
+        keys[6] = 1
+    if vel_msg.linear.y == 1:
+        keys[0] = 1
+    if vel_msg.linear.x == 1:
+        keys[7] = 1
+    if vel_msg.linear.y == -1:
+        keys[5] = 1
+    return keys
+
+def pub_image(env):
+    #GYM RENDER AS IMAGE
+    img = env.render(mode='rgb_array')
+    # ROTATE THE IMAGE THE MATRIX IS 90 grates and mirror
+    img = np.flipud(np.rot90(img))
+    image_np = imutils.resize(img, width=500)
+    # Publish new image
+    msg = CompressedImage()
+    msg.header.stamp = rospy.Time.now()
+    msg.format = "jpeg"
+    compressed_images = cv2.imencode('.jpg', image_np)
+    msg.data = np.array(compressed_images[1]).tostring()
+    image_pub.publish(msg)
+
+def open_world(vel_msg):
+    pub_image(env)
+    action = key_action(vel_msg)
+    obs, rew, done, info = env.step(action)
 
 if __name__ == '__main__':
+    rospy.init_node('world_observation_server', anonymous=True)
     try:
-        open_world()
+        rospy.Subscriber("world_observation/cmd_vel", Twist, open_world)
+        env.reset()
+        pub_image(env)
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
--- viewer.py
+++ viewer.py
@@ -6,24 +6,46 @@ import numpy as np
 import sys
 from std_msgs.msg import String
 from sensor_msgs.msg import CompressedImage
+from geometry_msgs.msg import Twist
 import pygame
+from pygame.locals import *
 import cv2
 
 video_size = 700, 500
-screen = pygame.display.set_mode(video_size)
+velocity_publisher = rospy.Publisher('world_observation/cmd_vel', Twist, queue_size=10)
+
+def key_action():
+    vel_msg = Twist()
+    keys=pygame.key.get_pressed()
+    if keys[K_LEFT]:
+        vel_msg.linear.x = -1
+    if keys[K_UP]:
+        vel_msg.linear.y = 1
+    if keys[K_RIGHT]:
+        vel_msg.linear.x = 1
+    if keys[K_DOWN]:
+        vel_msg.linear.y = -1
+    return vel_msg
+
 
 def callback(ros_data):
     np_arr = np.fromstring(ros_data.data, np.uint8)
     image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    screen = pygame.display.set_mode(video_size)
     surf = pygame.surfarray.make_surface(image_np)
     screen.blit(surf, (0, 0))
     pygame.display.update()
+    vel_msg = key_action()
+    velocity_publisher.publish(vel_msg)
 
 def main(args):
     '''Initializes and cleanup ros node'''
-    rospy.init_node('gym_viewer', anonymous=True)
+    rospy.init_node('world_observation_client', anonymous=True)
     subscriber = rospy.Subscriber('world_observation/image_raw', CompressedImage, callback)
     try:
+        screen = pygame.display.set_mode(video_size)
+        vel_msg = key_action()
+        velocity_publisher.publish(vel_msg)
         rospy.spin()
     except KeyboardInterrupt:
         print("Shutting down ROS Gym Image Viewer module")
